00360a27859d02f2c643d5ff72e9001b330e4c88
Sophia Willows <20146550+sophiabits@users.noreply.github.com>
2022-04-30 16:50:38 +1200

    Acquire Notion API key for testing reads

---
+++ config/jest.setup.js
@@ -0,0 +1,1 @@
+process.env.NOTION_TOKEN = 'secret_dT1OKyIjqKd2uzYhYQdinglbN0HhyVPZmzd0aZcZp2M';
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
   roots: ['<rootDir>/src'],
+  setupFiles: ['./config/jest.setup.js'],
   testRegex: '__tests__/.*\\.test\\.tsx?$',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
