25f4d703cfcd60cd42f7d9095cb665b49ddd3564
Sophia Willows <20146550+sophiabits@users.noreply.github.com>
2022-05-01 09:50:07 +1200

    Improve Filter type

--- src/types.ts
+++ src/types.ts
@@ -5,11 +5,8 @@ export interface BaseEntity {
 }
 
 export type Filter<EntityType extends BaseEntity> = {
-  [Prop in keyof EntityType]?: Condition<EntityType[Prop]>;
+  [Prop in keyof EntityType]?: FilterSelector<EntityType[Prop]>;
 };
-
-type ApplyBasicQueryCasting<T> = T | T[] | any;
-type Condition<T> = FilterSelector<ApplyBasicQueryCasting<T>>;
 
 // Stolen from mongoose :)
 // See: https://github.com/Automattic/mongoose/blob/f7fe2c7dae23e0702f554965e0055c5b27eadbfb/types/index.d.ts#L2048
@@ -17,9 +14,9 @@ export type FilterSelector<T = any> = {
   $eq?: T;
   $gt?: T;
   $gte?: T;
-  $in?: [T] extends AnyArray<any> ? ExtractArrayType<T>[] : T[];
+  $in?: T extends AnyArray<any> ? ExtractArrayType<T>[] : T[];
   $lt?: T;
   $lte?: T;
   $ne?: T;
-  $nin?: [T] extends AnyArray<any> ? ExtractArrayType<T>[] : T[];
+  $nin?: T extends AnyArray<any> ? ExtractArrayType<T>[] : T[];
 };
